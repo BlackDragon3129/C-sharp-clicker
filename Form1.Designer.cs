2fbac0e3fc16cc6cc3615fb94a373bf8b9e3e19f
BlackDragon3129 <dayte.akkaunt.2010@gmail.com>
2024-06-30 11:38:49 +0300

    Here's only russian

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Clicker
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.MoneyLabel = new System.Windows.Forms.Label();
+            this.PerClickLabel = new System.Windows.Forms.Label();
+            this.PerSecondLabel = new System.Windows.Forms.Label();
+            this.ClickCostLabel = new System.Windows.Forms.Label();
+            this.SecCostLabel = new System.Windows.Forms.Label();
+            this.XLabel = new System.Windows.Forms.Label();
+            this.ClickButton = new System.Windows.Forms.Button();
+            this.ClickUpgrade = new System.Windows.Forms.Button();
+            this.SecUpgrade = new System.Windows.Forms.Button();
+            this.ProgressDropButton = new System.Windows.Forms.Button();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SecondTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // MoneyLabel
+            // 
+            this.MoneyLabel.AutoSize = true;
+            this.MoneyLabel.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MoneyLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MoneyLabel.Location = new System.Drawing.Point(35, 25);
+            this.MoneyLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.MoneyLabel.Name = "MoneyLabel";
+            this.MoneyLabel.Size = new System.Drawing.Size(54, 38);
+            this.MoneyLabel.TabIndex = 0;
+            this.MoneyLabel.Text = "☺";
+            // 
+            // PerClickLabel
+            // 
+            this.PerClickLabel.AutoSize = true;
+            this.PerClickLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PerClickLabel.Location = new System.Drawing.Point(35, 100);
+            this.PerClickLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.PerClickLabel.Name = "PerClickLabel";
+            this.PerClickLabel.Size = new System.Drawing.Size(168, 38);
+            this.PerClickLabel.TabIndex = 1;
+            this.PerClickLabel.Text = "За клик: ☺";
+            // 
+            // PerSecondLabel
+            // 
+            this.PerSecondLabel.AutoSize = true;
+            this.PerSecondLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PerSecondLabel.Location = new System.Drawing.Point(35, 225);
+            this.PerSecondLabel.Name = "PerSecondLabel";
+            this.PerSecondLabel.Size = new System.Drawing.Size(204, 38);
+            this.PerSecondLabel.TabIndex = 2;
+            this.PerSecondLabel.Text = "В секунду:  ☺";
+            // 
+            // ClickCostLabel
+            // 
+            this.ClickCostLabel.AutoSize = true;
+            this.ClickCostLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClickCostLabel.Location = new System.Drawing.Point(35, 150);
+            this.ClickCostLabel.Name = "ClickCostLabel";
+            this.ClickCostLabel.Size = new System.Drawing.Size(329, 38);
+            this.ClickCostLabel.TabIndex = 3;
+            this.ClickCostLabel.Text = "Цена улучшения клика: ";
+            // 
+            // SecCostLabel
+            // 
+            this.SecCostLabel.AutoSize = true;
+            this.SecCostLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.SecCostLabel.Location = new System.Drawing.Point(35, 275);
+            this.SecCostLabel.Name = "SecCostLabel";
+            this.SecCostLabel.Size = new System.Drawing.Size(423, 38);
+            this.SecCostLabel.TabIndex = 4;
+            this.SecCostLabel.Text = "Цена улучшения ☺ в секунду: ";
+            // 
+            // XLabel
+            // 
+            this.XLabel.AutoSize = true;
+            this.XLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.XLabel.Location = new System.Drawing.Point(35, 350);
+            this.XLabel.Name = "XLabel";
+            this.XLabel.Size = new System.Drawing.Size(208, 38);
+            this.XLabel.TabIndex = 5;
+            this.XLabel.Text = "Множитель: x1";
+            // 
+            // ClickButton
+            // 
+            this.ClickButton.BackColor = System.Drawing.Color.Black;
+            this.ClickButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
+            this.ClickButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClickButton.Location = new System.Drawing.Point(35, 425);
+            this.ClickButton.Name = "ClickButton";
+            this.ClickButton.Size = new System.Drawing.Size(150, 50);
+            this.ClickButton.TabIndex = 6;
+            this.ClickButton.Text = "Кликай!";
+            this.ClickButton.UseVisualStyleBackColor = false;
+            this.ClickButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ClickButton_MouseClick);
+            // 
+            // ClickUpgrade
+            // 
+            this.ClickUpgrade.BackColor = System.Drawing.Color.Black;
+            this.ClickUpgrade.Font = new System.Drawing.Font("Comic Sans MS", 12.25F);
+            this.ClickUpgrade.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClickUpgrade.Location = new System.Drawing.Point(215, 425);
+            this.ClickUpgrade.Name = "ClickUpgrade";
+            this.ClickUpgrade.Size = new System.Drawing.Size(150, 50);
+            this.ClickUpgrade.TabIndex = 7;
+            this.ClickUpgrade.Text = "Улучшить клик";
+            this.ClickUpgrade.UseVisualStyleBackColor = false;
+            this.ClickUpgrade.Click += new System.EventHandler(this.ClickUpgrade_Click);
+            // 
+            // SecUpgrade
+            // 
+            this.SecUpgrade.BackColor = System.Drawing.Color.Black;
+            this.SecUpgrade.Font = new System.Drawing.Font("Comic Sans MS", 11.25F);
+            this.SecUpgrade.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.SecUpgrade.Location = new System.Drawing.Point(395, 425);
+            this.SecUpgrade.Name = "SecUpgrade";
+            this.SecUpgrade.Size = new System.Drawing.Size(150, 50);
+            this.SecUpgrade.TabIndex = 8;
+            this.SecUpgrade.Text = "Улучшить секунду";
+            this.SecUpgrade.UseVisualStyleBackColor = false;
+            this.SecUpgrade.Click += new System.EventHandler(this.SecUpgrade_Click);
+            // 
+            // ProgressDropButton
+            // 
+            this.ProgressDropButton.BackColor = System.Drawing.Color.Black;
+            this.ProgressDropButton.Enabled = false;
+            this.ProgressDropButton.Font = new System.Drawing.Font("Comic Sans MS", 11.25F);
+            this.ProgressDropButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ProgressDropButton.Location = new System.Drawing.Point(35, 500);
+            this.ProgressDropButton.Name = "ProgressDropButton";
+            this.ProgressDropButton.Size = new System.Drawing.Size(150, 50);
+            this.ProgressDropButton.TabIndex = 9;
+            this.ProgressDropButton.Text = "Сбросить прогресс";
+            this.ProgressDropButton.UseVisualStyleBackColor = false;
+            this.ProgressDropButton.Click += new System.EventHandler(this.ProgressDropButton_Click);
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.BackColor = System.Drawing.Color.Black;
+            this.NewGameButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
+            this.NewGameButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.NewGameButton.Location = new System.Drawing.Point(215, 500);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(150, 50);
+            this.NewGameButton.TabIndex = 10;
+            this.NewGameButton.Text = "Новая игра";
+            this.NewGameButton.UseVisualStyleBackColor = false;
+            this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Black;
+            this.ExitButton.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
+            this.ExitButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ExitButton.Location = new System.Drawing.Point(395, 500);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(150, 50);
+            this.ExitButton.TabIndex = 11;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // SecondTimer
+            // 
+            this.SecondTimer.Enabled = true;
+            this.SecondTimer.Interval = 1000;
+            this.SecondTimer.Tick += new System.EventHandler(this.SecondTimer_Tick);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.NewGameButton);
+            this.Controls.Add(this.ProgressDropButton);
+            this.Controls.Add(this.SecUpgrade);
+            this.Controls.Add(this.ClickUpgrade);
+            this.Controls.Add(this.ClickButton);
+            this.Controls.Add(this.XLabel);
+            this.Controls.Add(this.SecCostLabel);
+            this.Controls.Add(this.ClickCostLabel);
+            this.Controls.Add(this.PerSecondLabel);
+            this.Controls.Add(this.PerClickLabel);
+            this.Controls.Add(this.MoneyLabel);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.Name = "Window";
+            this.Text = "Clicker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Window_FormClosing);
+            this.Load += new System.EventHandler(this.Window_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MoneyLabel;
+        private System.Windows.Forms.Label PerClickLabel;
+        private System.Windows.Forms.Label PerSecondLabel;
+        private System.Windows.Forms.Label ClickCostLabel;
+        private System.Windows.Forms.Label SecCostLabel;
+        private System.Windows.Forms.Label XLabel;
+        private System.Windows.Forms.Button ClickButton;
+        private System.Windows.Forms.Button ClickUpgrade;
+        private System.Windows.Forms.Button SecUpgrade;
+        private System.Windows.Forms.Button ProgressDropButton;
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Timer SecondTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Window : Form
+    {
+        private float _money = 0f;       // текущее кол-во монет
+        private float _progress = 1f;    // множитель монет
+        private ulong _perClick = 1;     // монет за клик
+        private ulong _clickCost = 100;  // цена за улучшение клика
+        private ulong _perSecond = 0;    // монет в секунду
+        private ulong _secCost = 1000;    // цена за улучшение кол-ва монет в секунду
+
+        // функция сохранения
+        private void Save()
+        {
+            if (MessageBox.Show("Сохранить данные?", "Сохранение", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // создание папки
+                Directory.CreateDirectory("C:\\ProgramData\\BlackDragon3129\\Clicker");
+
+                // текст для записи
+                string text = "Hi! It's BlackDragon3129. What are you doing here? Are you a hacker? " +
+                        "Here's some numbers. Please, don't touch anything! See you later!\n" +
+                        _money.ToString() + '\n' +
+                        _progress.ToString() + '\n' +
+                        _perClick.ToString() + '\n' +
+                        _clickCost.ToString() + '\n' +
+                        _perSecond.ToString() + '\n' +
+                        _secCost.ToString();
+
+                // запись в файл
+                File.WriteAllText("C:\\ProgramData\\BlackDragon3129\\Clicker\\data.ЭтоРасширениеБесполезноЗачемОно",
+                    text);
+            }
+        }
+
+        // конвертатор в буквы
+        private string ToLetterConverter(ulong value)
+        {
+            string letterValue;
+            if (value / 1_000_000_000_000f > 1f)
+                letterValue = Math.Round((decimal)value / 1_000_000_000_000, 2).ToString() + "T";
+            else if (value / 1_000_000_000f > 1f)
+                letterValue = Math.Round((decimal)value / 1_000_000_000, 2).ToString() + "B";
+            else if (value / 1_000_000f > 1f)
+                letterValue = Math.Round((decimal)value / 1_000_000, 2).ToString() + "M";
+            else if (value / 1_000f > 1f)
+                letterValue = Math.Round((decimal)value / 1_000, 2).ToString() + "K";
+            else
+                letterValue = value.ToString();
+
+            return letterValue;
+        }
+        private string ToLetterConverter(float value)
+        {
+            string letterValue;
+            if (value / 1_000_000_000_000f > 1f)
+                letterValue = Math.Round((decimal)value / 1_000_000_000_000, 2).ToString() + "T";
+            else if (value / 1_000_000_000f > 1f)
+                letterValue = Math.Round((decimal)value / 1_000_000_000, 2).ToString() + "B";
+            else if (value / 1_000_000f > 1f)
+                letterValue = Math.Round((decimal)value / 1_000_000, 2).ToString() + "M";
+            else if (value / 1_000f > 1f)
+                letterValue = Math.Round((decimal)value / 1_000, 2).ToString() + "K";
+            else
+                letterValue = value.ToString();
+
+            return letterValue;
+        }
+
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        // загрузка
+        private void Window_Load(object sender, EventArgs e)
+        {
+            // загрузка из файла
+            try
+            {
+                string[] values =
+                    File.ReadAllLines("C:\\ProgramData\\BlackDragon3129\\Clicker\\data.ЭтоРасширениеБесполезноЗачемОно");
+                // попытка взлома
+                if (values[0] != "Hi! It's BlackDragon3129. What are you doing here? Are you a hacker? " +
+                    "Here's some numbers. Please, don't touch anything! See you later!")
+                {
+                    _money = 0f;
+                    _progress = 0.1f;
+                    _perClick = 0;
+                    _clickCost = 999_999_999_999;
+                    _perSecond = 0;
+                    _secCost = 999_999_999_999;
+
+                    File.WriteAllText("C:\\ProgramData\\BlackDragon3129\\Clicker\\data.ЭтоРасщирениеБесполезноЗачемОно",
+                        "Hi, ******* hacker!\n0\n0.1\n0\n999999999999\n0\n999999999999");
+                }
+                // всё хорошо
+                else
+                {
+                    _money = Convert.ToSingle(values[1]);
+                    _progress = Convert.ToSingle(values[2]);
+                    _perClick = Convert.ToUInt64(values[3]);
+                    _clickCost = Convert.ToUInt64(values[4]);
+                    _perSecond = Convert.ToUInt64(values[5]);
+                    _secCost = Convert.ToUInt64(values[6]);
+                }
+            }
+            // файл не найден
+            catch
+            {
+                _money = 0f;
+                _progress = 1f;
+                _perClick = 1;
+                _clickCost = 100;
+                _perSecond = 0;
+                _secCost = 1000;
+            }
+            // синхронизация
+            finally
+            {
+                // монеты
+
+                MoneyLabel.Text = ToLetterConverter(_money) + " ☺";
+
+                // клик
+                PerClickLabel.Text = "За клик: " + ToLetterConverter(_perClick) + " ☺";
+                ClickCostLabel.Text = "Цена улучшения клика: " + ToLetterConverter(_clickCost) + " ☺";
+
+                // в секунду
+                PerSecondLabel.Text = "В секунду: " + ToLetterConverter(_perSecond) + " ☺";
+                SecCostLabel.Text = "Цена улучшения секунды: " + ToLetterConverter(_secCost) + " ☺";
+
+                // множитель
+                XLabel.Text = "Множитель: x" + _progress.ToString();
+
+                // блок/разблокировка кнопок клика и прогресса
+                if (_money >= 100_000_000_000f)
+                    ProgressDropButton.Enabled = true;
+                if (_money + _perClick > ulong.MaxValue)
+                    ClickButton.Enabled = false;
+            }
+        }
+
+        // сохранение
+        private void Window_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (File.ReadAllLines("C:\\ProgramData\\BlackDragon3129\\Clicker\\data.ЭтоРасширениеБесполезноЗачемОно")[0]
+                    == "Hi! It's BlackDragon3129. What are you doing here? Are you a hacker? " +
+                        "Here's some numbers. Please, don't touch anything! See you later!")
+                {
+                    Save();
+                }
+            }
+            catch
+            {
+                Save(); 
+            }
+        }
+
+        // улучшение клика
+        private void ClickUpgrade_Click(object sender, EventArgs e)
+        {
+            // расчёты
+            if (_money >= _clickCost)
+            {
+                // уменьшение монет
+                _money -= _clickCost;
+
+                // подорожание клика
+                _perClick ++;
+
+                // перерасчёт стоимости
+                Random random = new Random();
+                _clickCost = _perClick * (ulong)random.Next(90, 111) * _perClick;
+            }
+
+            // разблокировка кнопки клика
+            if (_money + _perClick > ulong.MaxValue)
+                ClickButton.Enabled = true;
+
+            // синхронизация
+            MoneyLabel.Text = ToLetterConverter(_money) + " ☺";
+            ClickCostLabel.Text = "Цена улучшения клика: " + ToLetterConverter(_clickCost) + " ☺";
+            PerClickLabel.Text = "За клик: " + ToLetterConverter(_perClick) + " ☺";
+        }
+
+        // выход
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // новая игра
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены все заначения (включая множитель) вернутся к базовым!", "Точно?",
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // переопределение занчений
+                _money = 0f;
+                _progress = 1f;
+                _perClick = 1;
+                _clickCost = 100;
+                _perSecond = 0;
+                _secCost = 1000;
+
+                // удаление сохранения
+                if (File.Exists("C:\\ProgramData\\BlackDragon3129\\Clicker\\data.ЭтоРасширениеБесполезноЗачемОно"))
+                    File.Delete("C:\\ProgramData\\BlackDragon3129\\Clicker\\data.ЭтоРасширениеБесполезноЗачемОно");
+
+                // синхронизация
+                MoneyLabel.Text = "0 ☺";
+                PerClickLabel.Text = "За клик: 1 ☺";
+                ClickCostLabel.Text = "Цена улучшения клика: 100 ☺";
+                PerSecondLabel.Text = "В секунду: 0 ☺";
+                SecCostLabel.Text = "Цена улучшения секунды: 100 ☺";
+                XLabel.Text = "Множитель: x1";
+            }
+        }
+
+        // сброс прогресса
+        private void ProgressDropButton_Click(object sender, EventArgs e)
+        {
+            // обновление множителя
+            _progress += 0.2f;
+
+            // сброс значений
+            _money = 0f;
+            _perClick = 1;
+            _clickCost = 100;
+            _perSecond = 0;
+            _secCost = 100;
+
+            // синхронизация
+            MoneyLabel.Text = "0 ☺";
+            PerClickLabel.Text = "За клик: 1 ☺";
+            ClickCostLabel.Text = "Цена улучшения клика: 100 ☺";
+            PerSecondLabel.Text = "В секунду: 0 ☺";
+            SecCostLabel.Text = "Цена улучшения секунды: 100 ☺";
+            XLabel.Text = "Множитель: x" + _progress.ToString();
+        }
+
+        // клик
+        private void ClickButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            // прибавка монет
+            _money += _perClick * _progress;
+
+            // разблокировка сброса прогресса
+            ulong roundMoney = ((ulong)Math.Round((decimal)_money)); ;
+            if (roundMoney >= 100_000_000_000)
+                ProgressDropButton.Enabled = true;
+
+            // блокировка, если при следуюущем нажатии переполнение
+            if (_money + _perClick > ulong.MaxValue)
+                ClickButton.Enabled = false;
+
+            // синхронизация
+            MoneyLabel.Text = ToLetterConverter(_money) + " ☺";
+        }
+
+        private void SecondTimer_Tick(object sender, EventArgs e)
+        {
+            // прибавка монет
+            _money += _perSecond * _progress;
+
+            // синхронизация
+            MoneyLabel.Text = ToLetterConverter(_money) + " ☺";
+        }
+
+        private void SecUpgrade_Click(object sender, EventArgs e)
+        {
+            // расчёты
+            if (_money >= _secCost)
+            {
+                // уменьшение монет
+                _money -= _secCost;
+
+                // подорожание секунды
+                _perSecond++;
+
+                // перерасчёт стоимости
+                Random random = new Random();
+                _secCost = _perSecond * (ulong)random.Next(90, 111) * _perClick * 10;
+            }
+
+            // разблокировка кнопки клика
+            if (_money + _perClick > ulong.MaxValue)
+                ClickButton.Enabled = true;
+
+            // синхронизация
+            MoneyLabel.Text = ToLetterConverter(_money) + " ☺";
+            PerSecondLabel.Text = "В секунду: " + ToLetterConverter(_perSecond) + " ☺";
+            SecCostLabel.Text = "Цена улучшения ☺ в секунду: " + ToLetterConverter(_secCost) + " ☺";
+        }
+    }
+}
